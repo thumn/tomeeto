e083550c7d3813b15f78ccd9657266a73b5afa8e
Elise Xu <elisexu98@gmail.com>
2018-01-20 15:24:22 -0800

    changed to hello, attempting to deploy on elise's machine

--- hackdavis/hello_world/main.py
+++ hackdavis/hello_world/main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, bitches! Version 2')
+        self.response.write('Hello! Version 2')
 
 
 app = webapp2.WSGIApplication([
