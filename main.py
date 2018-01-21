645218a28788434e3a702fad64adca88791a4ae7
dengjulie <juliedeng@berkeley.edu>
2018-01-20 21:26:07 -0800

    adding photo upload functions

--- main.py
+++ main.py
@@ -14,13 +14,18 @@
 
 # [START app]
 import logging
+import os
 
 # [START imports]
 from flask import Flask, render_template, request
 # [END imports]
 
+UPLOAD_FOLDER = './img'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 # [START create_app]
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # [END create_app]
 
 @app.route('/')
@@ -39,6 +44,25 @@ def dropdown():
     years = ['Freshman','Sophomore','Junior','Senior','Super Senior!!11!!'];
     return render_template('/basicinfo.html', years=years)
 
+@app.route('/photo')
+def photo():
+    return render_template('photo.html')
+
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
 # [START submitted]
 @app.route('/submitted', methods=['POST'])
